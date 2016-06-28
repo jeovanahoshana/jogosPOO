bf1c1ce83b208d56227785423993dcb91d97d42f
jeovanahoshana <jeovaejeovana@gmail.com>
2016-06-27 21:07:41 -0300

    jogosPOO

--- hello world.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> // biblioteca
-using namespace std;
-int main(){ //função principal
-
-	cout << "Hello World" << endl; //imprime o nome "Hello world na tela"
-
-	return 0; //termina o programa
-}
